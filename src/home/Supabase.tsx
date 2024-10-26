3b0a2034b355b931b8d95e386241c828ae90498a
Hamza Wahbi <hamzawahbi1@outlook.com>
2024-10-27 00:09:56 +0100

    added

--- src/home/Supabase.tsx
+++ src/home/Supabase.tsx
@@ -4,7 +4,7 @@ import parserLogic from './parserLogic';
 
 // supabase client keys and url
 const supabaseUrl = "https://zzbgglheuqzbwueaeino.supabase.co/";
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6YmdnbGhldXF6Ynd1ZWFlaW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NTMxNDYsImV4cCI6MjA0NTUyOTE0Nn0.hrWh4QXGfrol8LFHd5_AjryOZL8t4kEsDM25ITXH2xo";
+const apiKey = "";
 
 // supabase client
 const supabaseClient = createClient(supabaseUrl, apiKey);
